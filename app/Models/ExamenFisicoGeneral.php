6143011803193e437eb7e4bbcef0b226890da4cc
GiancarloGC <dannygiancarlogc@gmail.com>
2022-05-04 10:39:00 -0500

    examen_fisico_general  migration

--- app/Models/ExamenFisicoGeneral.php
+++ app/Models/ExamenFisicoGeneral.php
@@ -8,4 +8,54 @@ use Illuminate\Database\Eloquent\Model;
 class ExamenFisicoGeneral extends Model
 {
     use HasFactory;
+    protected $table = 'examen_fisico_general';
+
+    protected $fillable = [
+        'historia_medica_id',
+
+        'CraneoExamenFisicoGeneral',
+        'ParescranealesExamenFisicoGeneral',
+        'ParpadosExamenFisicoGeneral',
+        'ConjuntivasExamenFisicoGeneral',
+        'PupilasExamenFisicoGeneral',
+        'FondodeojoExamenFisicoGeneral',
+        'PabellonExamenFisicoGeneral',
+        'ConductosExamenFisicoGeneral',
+        'TimpanosExamenFisicoGeneral',
+        'TabiqueExamenFisicoGeneral',
+        'CornetasMucosasExamenFisicoGeneral',
+        'LabiosExamenFisicoGeneral',
+        'LenguaExamenFisicoGeneral',
+        'DientesExamenFisicoGeneral',
+        'PaladarExamenFisicoGeneral',
+        'AmigdalasExamenFisicoGeneral',
+        'CuelloExamenFisicoGeneral',
+        'ToraxExamenFisicoGeneral',
+        'EspaldaExamenFisicoGeneral',
+        'PielExamenFisicoGeneral',
+
+
+        'CardiacoExamenFisicoGeneral',
+        'CirculatorioExamenFisicoGeneral',
+        'PulmonarExamenFisicoGeneral',
+        'SenosExamenFisicoGeneral',
+        'AbdomenaspectoExamenFisicoGeneral',
+        'ViscerasExamenFisicoGeneral',
+        'GenitalesexternosExamenFisicoGeneral',
+        'HerniasExamenFisicoGeneral',
+        'AdenopatiasExamenFisicoGeneral',
+        'MiembrossuperioresExamenFisicoGeneral',
+        'MiembrosinferioresExamenFisicoGeneral',
+        'VascularperifericoExamenFisicoGeneral',
+        'NeurologicoExamenFisicoGeneral',
+        'ReflejostendinososExamenFisicoGeneral',
+        'MarchaExamenFisicoGeneral',
+        'EquilibrioExamenFisicoGeneral',
+        'EstadomentalExamenFisicoGeneral',
+        'CicatricesExamenFisicoGeneral',
+        'GrupoExamenFisicoGeneral',
+
+
+        'AmpliacionExamenFisicoGeneral'
+    ];
 }
--- database/migrations/2022_05_02_161623_create_examen_fisico_generals_table.php
+++ database/migrations/2022_05_02_161623_create_examen_fisico_generals_table.php
@@ -13,8 +13,56 @@ class CreateExamenFisicoGeneralsTable extends Migration
      */
     public function up()
     {
-        Schema::create('examen_fisico_generals', function (Blueprint $table) {
+        Schema::create('examen_fisico_general', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('historia_medica_id');
+
+            $table->boolean('CraneoExamenFisicoGeneral')->default(1);
+            $table->boolean('ParescranealesExamenFisicoGeneral')->default(1);
+            $table->boolean('ParpadosExamenFisicoGeneral')->default(1);
+            $table->boolean('ConjuntivasExamenFisicoGeneral')->default(1);
+            $table->boolean('PupilasExamenFisicoGeneral')->default(1);
+            $table->boolean('FondodeojoExamenFisicoGeneral')->default(1);
+            $table->boolean('PabellonExamenFisicoGeneral')->default(1);
+            $table->boolean('ConductosExamenFisicoGeneral')->default(1);
+            $table->boolean('TimpanosExamenFisicoGeneral')->default(1);
+            $table->boolean('TabiqueExamenFisicoGeneral')->default(1);
+            $table->boolean('CornetasMucosasExamenFisicoGeneral')->default(1);
+            $table->boolean('LabiosExamenFisicoGeneral')->default(1);
+            $table->boolean('LenguaExamenFisicoGeneral')->default(1);
+            $table->boolean('DientesExamenFisicoGeneral')->default(1);
+            $table->boolean('PaladarExamenFisicoGeneral')->default(1);
+            $table->boolean('AmigdalasExamenFisicoGeneral')->default(1);
+            $table->boolean('CuelloExamenFisicoGeneral')->default(1);
+            $table->boolean('ToraxExamenFisicoGeneral')->default(1);
+            $table->boolean('EspaldaExamenFisicoGeneral')->default(1);
+            $table->boolean('PielExamenFisicoGeneral')->default(1);
+
+
+            $table->boolean('CardiacoExamenFisicoGeneral')->default(1);
+            $table->boolean('CirculatorioExamenFisicoGeneral')->default(1);
+            $table->boolean('PulmonarExamenFisicoGeneral')->default(1);
+            $table->boolean('SenosExamenFisicoGeneral')->default(1);
+            $table->boolean('AbdomenaspectoExamenFisicoGeneral')->default(1);
+            $table->boolean('ViscerasExamenFisicoGeneral')->default(1);
+            $table->boolean('GenitalesexternosExamenFisicoGeneral')->default(1);
+            $table->boolean('HerniasExamenFisicoGeneral')->default(1);
+            $table->boolean('AdenopatiasExamenFisicoGeneral')->default(1);
+            $table->boolean('MiembrossuperioresExamenFisicoGeneral')->default(1);
+            $table->boolean('MiembrosinferioresExamenFisicoGeneral')->default(1);
+            $table->boolean('VascularperifericoExamenFisicoGeneral')->default(1);
+            $table->boolean('NeurologicoExamenFisicoGeneral')->default(1);
+            $table->boolean('ReflejostendinososExamenFisicoGeneral')->default(1);
+            $table->boolean('MarchaExamenFisicoGeneral')->default(1);
+            $table->boolean('EquilibrioExamenFisicoGeneral')->default(1);
+            $table->boolean('EstadomentalExamenFisicoGeneral')->default(1);
+            $table->boolean('CicatricesExamenFisicoGeneral')->default(1);
+            $table->boolean('GrupoExamenFisicoGeneral')->default(1);
+
+
+            $table->text('AmpliacionExamenFisicoGeneral')->nullable();
+
+            $table->foreign('historia_medica_id')->references('id')->on('historia_medica')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -26,6 +74,6 @@ class CreateExamenFisicoGeneralsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('examen_fisico_generals');
+        Schema::dropIfExists('examen_fisico_general');
     }
 }
