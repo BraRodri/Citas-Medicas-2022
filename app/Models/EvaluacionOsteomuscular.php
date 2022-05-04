048f1120ba899a5cc47edad17378301d4d70eb24
GiancarloGC <dannygiancarlogc@gmail.com>
2022-05-04 11:45:46 -0500

    evaluacion_osteomuscular migration

--- app/Models/EvaluacionOsteomuscular.php
+++ app/Models/EvaluacionOsteomuscular.php
@@ -8,4 +8,49 @@ use Illuminate\Database\Eloquent\Model;
 class EvaluacionOsteomuscular extends Model
 {
     use HasFactory;
+
+    protected $table = 'evaluacion_osteomuscular';
+
+    protected $fillable = [
+        'historia_medica_id',
+
+        'hombrosEvaluacionOsteomuscularDer',
+        'hombrosEvaluacionOsteomuscularIzq',
+        'BrazosEvaluacionOsteomuscularDer',
+        'BrazosEvaluacionOsteomuscularIzq',
+        'CodosEvaluacionOsteomuscularDer',
+        'CodosEvaluacionOsteomuscularIzq',
+        'AntebrazosEvaluacionOsteomuscularDer',
+        'AntebrazosEvaluacionOsteomuscularIzq',
+        'MunecasEvaluacionOsteomuscularDer',
+        'MunecasEvaluacionOsteomuscularIzq',
+        'ManosEvaluacionOsteomuscularDer',
+        'ManosEvaluacionOsteomuscularIzq',
+
+        'CaderasEvaluacionOsteomuscularDer',
+        'CaderasEvaluacionOsteomuscularIzq',
+        'MuslosEvaluacionOsteomuscularDer',
+        'MuslosEvaluacionOsteomuscularIzq',
+        'RodillasEvaluacionOsteomuscularDer',
+        'RodillasEvaluacionOsteomuscularIzq',
+        'PiernasEvaluacionOsteomuscularDer',
+        'PiernasEvaluacionOsteomuscularIzq',
+        'TobillosEvaluacionOsteomuscularDer',
+        'TobillosEvaluacionOsteomuscularIzq',
+        'PiesEvaluacionOsteomuscularDer',
+        'PiesEvaluacionOsteomuscularIzq',
+
+        'TINNELEvaluacionOsteomuscularDer',
+        'TINNELEvaluacionOsteomuscularIzq',
+        'PHANELEvaluacionOsteomuscularDer',
+        'PHANELEvaluacionOsteomuscularIzq',
+        'REFLEJOSEvaluacionOsteomuscularDer',
+        'REFLEJOSEvaluacionOsteomuscularIzq',
+
+        'FUERZAMUSCULAREvaluacionOsteomuscular',
+        'SENSIBILIDADEvaluacionOsteomuscular',
+        'MOTILIDADEvaluacionOsteomuscular',
+
+        'AmpliacionEvaluacionOsteomuscular'
+    ];
 }
--- database/migrations/2022_05_02_161659_create_evaluacion_osteomusculars_table.php
+++ database/migrations/2022_05_02_161659_create_evaluacion_osteomusculars_table.php
@@ -13,9 +13,50 @@ class CreateEvaluacionOsteomuscularsTable extends Migration
      */
     public function up()
     {
-        Schema::create('evaluacion_osteomusculars', function (Blueprint $table) {
+        Schema::create('evaluacion_osteomuscular', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('historia_medica_id');
+
+            $table->boolean('hombrosEvaluacionOsteomuscularDer')->default(1);
+            $table->boolean('hombrosEvaluacionOsteomuscularIzq')->default(1);
+            $table->boolean('BrazosEvaluacionOsteomuscularDer')->default(1);
+            $table->boolean('BrazosEvaluacionOsteomuscularIzq')->default(1);
+            $table->boolean('CodosEvaluacionOsteomuscularDer')->default(1);
+            $table->boolean('CodosEvaluacionOsteomuscularIzq')->default(1);
+            $table->boolean('AntebrazosEvaluacionOsteomuscularDer')->default(1);
+            $table->boolean('AntebrazosEvaluacionOsteomuscularIzq')->default(1);
+            $table->boolean('MunecasEvaluacionOsteomuscularDer')->default(1);
+            $table->boolean('MunecasEvaluacionOsteomuscularIzq')->default(1);
+            $table->boolean('ManosEvaluacionOsteomuscularDer')->default(1);
+            $table->boolean('ManosEvaluacionOsteomuscularIzq')->default(1);
+
+            $table->boolean('CaderasEvaluacionOsteomuscularDer')->default(1);
+            $table->boolean('CaderasEvaluacionOsteomuscularIzq')->default(1);
+            $table->boolean('MuslosEvaluacionOsteomuscularDer')->default(1);
+            $table->boolean('MuslosEvaluacionOsteomuscularIzq')->default(1);
+            $table->boolean('RodillasEvaluacionOsteomuscularDer')->default(1);
+            $table->boolean('RodillasEvaluacionOsteomuscularIzq')->default(1);
+            $table->boolean('PiernasEvaluacionOsteomuscularDer')->default(1);
+            $table->boolean('PiernasEvaluacionOsteomuscularIzq')->default(1);
+            $table->boolean('TobillosEvaluacionOsteomuscularDer')->default(1);
+            $table->boolean('TobillosEvaluacionOsteomuscularIzq')->default(1);
+            $table->boolean('PiesEvaluacionOsteomuscularDer')->default(1);
+            $table->boolean('PiesEvaluacionOsteomuscularIzq')->default(1);
+
+            $table->boolean('TINNELEvaluacionOsteomuscularDer')->default(1);
+            $table->boolean('TINNELEvaluacionOsteomuscularIzq')->default(1);
+            $table->boolean('PHANELEvaluacionOsteomuscularDer')->default(1);
+            $table->boolean('PHANELEvaluacionOsteomuscularIzq')->default(1);
+            $table->boolean('REFLEJOSEvaluacionOsteomuscularDer')->default(1);
+            $table->boolean('REFLEJOSEvaluacionOsteomuscularIzq')->default(1);
+
+            $table->boolean('FUERZAMUSCULAREvaluacionOsteomuscular')->default(1);
+            $table->boolean('SENSIBILIDADEvaluacionOsteomuscular')->default(1);
+            $table->boolean('MOTILIDADEvaluacionOsteomuscular')->default(1);
+
+            $table->text('AmpliacionEvaluacionOsteomuscular');
             $table->timestamps();
+            $table->foreign('historia_medica_id')->references('id')->on('historia_medica')->onDelete('cascade');
         });
     }
 
@@ -26,6 +67,6 @@ class CreateEvaluacionOsteomuscularsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('evaluacion_osteomusculars');
+        Schema::dropIfExists('evaluacion_osteomuscular');
     }
 }
