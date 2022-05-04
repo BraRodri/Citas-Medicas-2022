6532bec7dc5850d99ca90cf33626708ae326b3bd
GiancarloGC <dannygiancarlogc@gmail.com>
2022-05-04 11:55:10 -0500

    evaluacion_de_columna migration

--- app/Models/EvaluacionDeColumna.php
+++ app/Models/EvaluacionDeColumna.php
@@ -8,4 +8,36 @@ use Illuminate\Database\Eloquent\Model;
 class EvaluacionDeColumna extends Model
 {
     use HasFactory;
+
+    protected $table = 'evaluacion_de_columna';
+
+    protected $fillable = [
+        'historia_medica_id',
+
+        'InspeccionEvaluacionDeColumna',
+        'PalpacionEvaluacionDeColumna',
+        'SensibilidadEvaluacionDeColumna',
+        'MotricidadEvaluacionDeColumna',
+        'EquilibrioestaticoEvaluacionDeColumna',
+
+        'AlineacionescapularEvaluacionDeColumna',
+        'AlineacionpelvicaEvaluacionDeColumna',
+        'CifosisEvaluacionDeColumna',
+        'LordosisEvaluacionDeColumna',
+        'EscoliosisEvaluacionDeColumna',
+
+        'FlexionanteriorEvaluacionDeColumna',
+        'ExtensionposteriorEvaluacionDeColumna',
+        'FlexionlateralEvaluacionDeColumna',
+        'RotacionEvaluacionDeColumna',
+        'MedicionextremidadesEvaluacionDeColumna',
+
+        'TestdeWellsEvaluacionDeColumna',
+        'PruebadeLassegueEvaluacionDeColumna',
+        'SignodeltimbreEvaluacionDeColumna',
+        'MarchaentalonesEvaluacionDeColumna',
+        'MarchaenpuntasEvaluacionDeColumna',
+
+        'AmpliacionEvaluacionDeColumna'
+    ];
 }
--- database/migrations/2022_05_02_161726_create_evaluacion_de_columnas_table.php
+++ database/migrations/2022_05_02_161726_create_evaluacion_de_columnas_table.php
@@ -13,9 +13,37 @@ class CreateEvaluacionDeColumnasTable extends Migration
      */
     public function up()
     {
-        Schema::create('evaluacion_de_columnas', function (Blueprint $table) {
+        Schema::create('evaluacion_de_columna', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('historia_medica_id');
+
+            $table->boolean('InspeccionEvaluacionDeColumna')->default(1);
+            $table->boolean('PalpacionEvaluacionDeColumna')->default(1);
+            $table->boolean('SensibilidadEvaluacionDeColumna')->default(1);
+            $table->boolean('MotricidadEvaluacionDeColumna')->default(1);
+            $table->boolean('EquilibrioestaticoEvaluacionDeColumna')->default(1);
+
+            $table->boolean('AlineacionescapularEvaluacionDeColumna')->default(1);
+            $table->boolean('AlineacionpelvicaEvaluacionDeColumna')->default(1);
+            $table->boolean('CifosisEvaluacionDeColumna')->default(1);
+            $table->boolean('LordosisEvaluacionDeColumna')->default(1);
+            $table->boolean('EscoliosisEvaluacionDeColumna')->default(1);
+
+            $table->boolean('FlexionanteriorEvaluacionDeColumna')->default(1);
+            $table->boolean('ExtensionposteriorEvaluacionDeColumna')->default(1);
+            $table->boolean('FlexionlateralEvaluacionDeColumna')->default(1);
+            $table->boolean('RotacionEvaluacionDeColumna')->default(1);
+            $table->boolean('MedicionextremidadesEvaluacionDeColumna')->default(1);
+
+            $table->boolean('TestdeWellsEvaluacionDeColumna')->default(1);
+            $table->boolean('PruebadeLassegueEvaluacionDeColumna')->default(1);
+            $table->boolean('SignodeltimbreEvaluacionDeColumna')->default(1);
+            $table->boolean('MarchaentalonesEvaluacionDeColumna')->default(1);
+            $table->boolean('MarchaenpuntasEvaluacionDeColumna')->default(1);
+
+            $table->text('AmpliacionEvaluacionDeColumna')->nullable();
             $table->timestamps();
+            $table->foreign('historia_medica_id')->references('id')->on('historia_medica')->onDelete('cascade');
         });
     }
 
@@ -26,6 +54,6 @@ class CreateEvaluacionDeColumnasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('evaluacion_de_columnas');
+        Schema::dropIfExists('evaluacion_de_columna');
     }
 }
