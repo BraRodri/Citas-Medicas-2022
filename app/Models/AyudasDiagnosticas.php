29e5fb3fa0dc0712d041441b3164105c721478d7
GiancarloGC <dannygiancarlogc@gmail.com>
2022-05-05 03:45:13 -0500

    ayudas_diagnosticas migration

--- app/Models/AyudasDiagnosticas.php
+++ app/Models/AyudasDiagnosticas.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class AyudasDiagnosticas extends Model
 {
     use HasFactory;
+
+    protected $table = 'ayudas_diagnosticas';
+
+    protected $fillable = [
+        'historia_medica_id',
+
+        'dateAgudezatamizExamen',
+        'OJODERECHOExamen',
+        'OJOIZQUIERDOExamen',
+        'ResultadoAgudezaExamen',
+
+        'dateAudiometriaTamizExamen',
+        'AudiometriaTamizExamen',
+        'ResultadoAudiometriaTamizExamen',
+
+        'dateAudiometriaclinicaExamen',
+        'SALExamen',
+        'ELIExamen',
+        'LARSENExamen',
+        'ResultadoAudiometriaclinicaExamen',
+
+        'dateEspirometriaExamen',
+        'EspirometriaFVCExamen',
+        'EspirometriaFEV1Examen',
+        'EspirometriaFEV1',
+        'ResultadoEspirometriaExamen'
+    ];
 }
--- database/migrations/2022_05_02_161822_create_ayudas_diagnosticas_table.php
+++ database/migrations/2022_05_02_161822_create_ayudas_diagnosticas_table.php
@@ -15,7 +15,31 @@ class CreateAyudasDiagnosticasTable extends Migration
     {
         Schema::create('ayudas_diagnosticas', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('historia_medica_id');
+
+            $table->date('dateAgudezatamizExamen')->nullable();
+            $table->string('OJODERECHOExamen')->nullable();
+            $table->string('OJOIZQUIERDOExamen')->nullable();
+            $table->string('ResultadoAgudezaExamen')->nullable();
+
+            $table->date('dateAudiometriaTamizExamen')->nullable();
+            $table->string('AudiometriaTamizExamen')->nullable();
+            $table->string('ResultadoAudiometriaTamizExamen')->nullable();
+
+            $table->date('dateAudiometriaclinicaExamen')->nullable();
+            $table->string('SALExamen')->nullable();
+            $table->string('ELIExamen')->nullable();
+            $table->string('LARSENExamen')->nullable();
+            $table->string('ResultadoAudiometriaclinicaExamen')->nullable();
+
+            $table->date('dateEspirometriaExamen')->nullable();
+            $table->string('EspirometriaFVCExamen')->nullable();
+            $table->string('EspirometriaFEV1Examen')->nullable();
+            $table->string('EspirometriaFEV1')->nullable();
+            $table->string('ResultadoEspirometriaExamen')->nullable();
+
             $table->timestamps();
+            $table->foreign('historia_medica_id')->references('id')->on('historia_medica')->onDelete('cascade');
         });
     }
 
