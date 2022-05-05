c29dbb7b306cfcb39d22350d8550eafbfe40318f
GiancarloGC <dannygiancarlogc@gmail.com>
2022-05-05 03:37:04 -0500

    examenes_de_laboratorio migration

--- app/Models/ExamenesDeLaboratorio.php
+++ app/Models/ExamenesDeLaboratorio.php
@@ -8,4 +8,36 @@ use Illuminate\Database\Eloquent\Model;
 class ExamenesDeLaboratorio extends Model
 {
     use HasFactory;
+
+    protected $table = 'examenes_de_laboratorio';
+
+    protected $fillable = [
+        'historia_medica_id',
+
+        'dateHemogramaExamen',
+        'HemoglobinaHemogramaExamen',
+        'HematocritoHemogramaExamen',
+
+        'dateGlicemiaExamen',
+        'GlicemiaExamen',
+
+        'datePERFILLIPIDICOExamen',
+        'ColesterolTotalExamen',
+        'ColesterolHDLExamen',
+        'ColesterolLDLExamen',
+        'TrigliceridosExamen',
+
+        'dateCitoQuimicoExamen',
+        'CitoQuimicoAspectoExamen',
+        'CitoQuimicoQuimicaExamen',
+        'CitoQuimicoMicroscopioExamen',
+
+        'dateOtrosExamen',
+        'otrosExamen',
+
+        'dateMANIPULADORESExamen',
+        'KOHMANIPULADORESExamen',
+        'FROTISMANIPULADORESExamen',
+        'COPROLOGICOMANIPULADORESExamen'
+    ];
 }
--- database/migrations/2022_05_02_161757_create_examenes_de_laboratorios_table.php
+++ database/migrations/2022_05_02_161757_create_examenes_de_laboratorios_table.php
@@ -13,9 +13,38 @@ class CreateExamenesDeLaboratoriosTable extends Migration
      */
     public function up()
     {
-        Schema::create('examenes_de_laboratorios', function (Blueprint $table) {
+        Schema::create('examenes_de_laboratorio', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('historia_medica_id');
+
+            $table->date('dateHemogramaExamen')->nullable();
+            $table->string('HemoglobinaHemogramaExamen')->nullable();
+            $table->string('HematocritoHemogramaExamen')->nullable();
+
+            $table->date('dateGlicemiaExamen')->nullable();
+            $table->string('GlicemiaExamen')->nullable();
+
+            $table->date('datePERFILLIPIDICOExamen')->nullable();
+            $table->string('ColesterolTotalExamen')->nullable();
+            $table->string('ColesterolHDLExamen')->nullable();
+            $table->string('ColesterolLDLExamen')->nullable();
+            $table->string('TrigliceridosExamen')->nullable();
+
+            $table->date('dateCitoQuimicoExamen')->nullable();
+            $table->string('CitoQuimicoAspectoExamen')->nullable();
+            $table->string('CitoQuimicoQuimicaExamen')->nullable();
+            $table->string('CitoQuimicoMicroscopioExamen')->nullable();
+
+            $table->date('dateOtrosExamen')->nullable();
+            $table->string('otrosExamen')->nullable();
+
+            $table->date('dateMANIPULADORESExamen')->nullable();
+            $table->string('KOHMANIPULADORESExamen')->nullable();
+            $table->string('FROTISMANIPULADORESExamen')->nullable();
+            $table->string('COPROLOGICOMANIPULADORESExamen')->nullable();
+
             $table->timestamps();
+            $table->foreign('historia_medica_id')->references('id')->on('historia_medica')->onDelete('cascade');
         });
     }
 
@@ -26,6 +55,6 @@ class CreateExamenesDeLaboratoriosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('examenes_de_laboratorios');
+        Schema::dropIfExists('examenes_de_laboratorio');
     }
 }
