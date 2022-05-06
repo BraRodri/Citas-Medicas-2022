5a3abf91376b7184d87a7126bbd1217dc882801c
GiancarloGC <dannygiancarlogc@gmail.com>
2022-05-06 07:33:39 -0500

    save in db ConductasYRecomendaciones

--- app/Http/Controllers/ConductasYRecomendacionesController.php
+++ app/Http/Controllers/ConductasYRecomendacionesController.php
@@ -36,7 +36,29 @@ class ConductasYRecomendacionesController extends Controller
      */
     static public function store(HistoriaMedica $historiaMedica, Request $request)
     {
-        //
+        ConductasYRecomendaciones::create([
+            'historia_medica_id' => $historiaMedica->id,
+
+            'checkRiesgospropiosdeloficioarealizar' => $request['checkRiesgospropiosdeloficioarealizar'] ? 1 : 0,
+            'checkUsodelosprotectoresauditivos' => $request['checkUsodelosprotectoresauditivos'] ? 1 : 0,
+            'checkUsodelaproteccionrespiratoria' => $request['checkUsodelaproteccionrespiratoria'] ? 1 : 0,
+            'checkEntrenamientodetrabajosenalturas' => $request['checkEntrenamientodetrabajosenalturas'] ? 1 : 0,
+
+            'checkSuministrarcapacitaryentrenarenelusodelosEPI' => $request['checkSuministrarcapacitaryentrenarenelusodelosEPI'] ? 1 : 0,
+            'checkUsoadecuadodelosEPIquelesuministrelaempresa' => $request['checkUsoadecuadodelosEPIquelesuministrelaempresa'] ? 1 : 0,
+            'checkUsodeproteccionnivelcondicionestrabajadorytipodetrabajo' => $request['checkUsodeproteccionnivelcondicionestrabajadorytipodetrabajo'] ? 1 : 0,
+            'checkUtilizarproteccionauditivaenambientesruidosostanto' => $request['checkUtilizarproteccionauditivaenambientesruidosostanto'] ? 1 : 0,
+
+            'checkUtilizarunaadecuadahigieneposturalymecanicacorporal' => $request['checkUtilizarunaadecuadahigieneposturalymecanicacorporal'] ? 1 : 0,
+            'checkEntrenamientoscontinuossobrelamecanicacorporal' => $request['checkEntrenamientoscontinuossobrelamecanicacorporal'] ? 1 : 0,
+            'checkLasmujeresnodebenmovilizarcargasdesdeelpiso' => $request['checkLasmujeresnodebenmovilizarcargasdesdeelpiso'] ? 1 : 0,
+            'checkHacerseguimientocontinuodelasrecomendacionespara' => $request['checkHacerseguimientocontinuodelasrecomendacionespara'] ? 1 : 0,
+
+            'checkConservacionAuditiva' => $request['checkConservacionAuditiva'] ? 1 : 0,
+            'checkPrevenciondelaaccidentalidadlaboral' => $request['checkPrevenciondelaaccidentalidadlaboral'] ? 1 : 0,
+            'checkPrevenciondepatologiasosteomusculares' => $request['checkPrevenciondepatologiasosteomusculares'] ? 1 : 0,
+            'checkPrevencionmanejodesustanciasquimicas' => $request['checkPrevencionmanejodesustanciasquimicas'] ? 1 : 0,
+        ]);
     }
 
     /**
